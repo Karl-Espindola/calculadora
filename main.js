60480e8a4f5793f4d6d78a8ffbdb21006534dd61
Karl <karlespindola2022@gmail.com>
2022-09-29 23:48:41 -0500

    commit 1

--- main.js
+++ main.js
@@ -1,49 +1,75 @@
-let val_1=0;
-let valAnterior=false;
+let numeros=[];
+let operador="";
 
+let val_1="";
+let valAnterior_1="";
+let valAnterior_2="";
+let val_resultado="";
 
-function capturar(){
-    val_1=parseFloat(val1.value);
+function ponerEnPantalla(i){
     
-}
-function visualizar(op){
-    if(val1.value!=""){
-        operador.innerText=op;
-        capturar();
-        valAnterior=val_1;
-        val1.value="";
-    }
-    
-}
-function AC(){
-    operador.innerText="";
-    val1.value="";
-    display.innerHTML="";
-}
-function resultado(){
-    if(operador.innerText!="" && val1.value!="" && valAnterior ){
-
-        capturar();
-        switch(operador.innerHTML){
-            case "+":
-                display.innerHTML=valAnterior+val_1;
-                break
-            case "-":
-                display.innerHTML=valAnterior-val_1;
-                break
-            case "x":
-                display.innerHTML=valAnterior*val_1;
-                break
-            case "/":
-                (val_1==0)?display.innerHTML="infinito":display.innerHTML=valAnterior/val_1;
-            break
-        }
+    if(operador=="" && val_resultado==""){
+        val_1=i;
+        valAnterior_1=display.innerHTML;
+        val_1=valAnterior_1+val_1;
+        display.innerText=val_1;
         
     }
+    if(operador!="" && val_resultado!=""){
+        val_1=val_resultado;
+    }
+    if(operador!=""){
+        val_2=i;
+        valAnterior_2=display.innerHTML;
+        val_2=valAnterior_2+val_2;
+        display.innerText=val_2;
+        
+    } 
 }
-let val1=document.getElementById("val-1");
+function capturar(){
+    val_1=parseFloat(display.innerText);
+    
+}
 
-let operador=document.getElementById("operador");
+function operacion(op){
+    operador=op;
+    display.innerHTML="";
+
+}
+
+function AC(){
+    display.innerHTML="";
+    val_1="";
+    val_2="";
+    valAnterior_1="";
+    valAnterior_2="";
+    val_resultado="";
+}
+function resultado(){
+    if(display.innerText!="" ){
+        val_1=parseFloat(val_1);
+        val_2=parseFloat(val_2);
+        
+        switch(operador){
+            case "+":
+                display.innerHTML=val_1+val_2;
+                break
+            case "-":
+                display.innerHTML=val_1-val_2;
+                break
+            case "x":
+                display.innerHTML=val_1*val_2;
+                break
+            case "/":
+                (val_2==0)?display.innerHTML="infinito":display.innerHTML=val_1/val_2;
+            break
+        }
+        operador="";
+        val_resultado=display.innerText;
+    }
+}
+
+
 let display=document.getElementById("display");
 
 let suma=document.getElementById("suma");
@@ -53,9 +79,16 @@ let division=document.getElementById("division");
 let igual=document.getElementById("igual");
 let borrar=document.getElementById("borrar");
 
-suma.addEventListener("click",()=>{visualizar("+")});
-resta.addEventListener("click",()=>{visualizar("-")});
-multi.addEventListener("click",()=>{visualizar("x")});
-division.addEventListener("click",()=>{visualizar("/")});
+suma.addEventListener("click",()=>{operacion("+")});
+resta.addEventListener("click",()=>{operacion("-")});
+multi.addEventListener("click",()=>{operacion("x")});
+division.addEventListener("click",()=>{operacion("/")});
 igual.addEventListener("click",resultado);
 borrar.addEventListener("click",AC);
+
+for(let i=0; i<=9; i++){
+    numeros[i]=document.getElementById("num"+i);
+    numeros[i].addEventListener("click",()=>{
+        ponerEnPantalla(i)
+    });
+}
